81bbe03da7c059138d8f76c35853171e42d6c231
mcdonald-o-m365group <mcdonald.o@m365connect.com>
2025-07-02 21:45:59 +0300

    shared

--- build/src/config.ts/config.js
+++ build/src/config.ts/config.js
@@ -4,8 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-process.env.NODE_ENV == 'production' ? dotenv_1.default.config({ path: '.env' }) : dotenv_1.default.config({ path: '.env.development' });
-console.log(process.env.NODE_ENV);
+dotenv_1.default.config();
+// process.env.RAILWAY_ENVIRONMENT == 'production'? dotenv.config({path:'.env'}) : dotenv.config({path:'.env.development'})
+console.log(process.env);
 const config = (envString) => {
     const env = process.env[envString];
     if (!env)
