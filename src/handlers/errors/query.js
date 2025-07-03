a9b04e45b6c5005ba040fbd4af3415f0723cb8be
mcdonald-o-m365group <mcdonald.o@m365connect.com>
2025-07-03 11:20:06 +0300

    shared

--- src/handlers/errors/query.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class InvalidQueryError extends Error {
-    constructor(queryType, query) {
-        super(`${queryType} query for query : ${query} is invalid or wrong`);
-    }
-}
-exports.default = InvalidQueryError;
--- src/routes/basic.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const controllers_1 = require("../handlers/controllers");
-const basicAuthRoutes = (0, express_1.Router)();
-basicAuthRoutes.post("/", controllers_1.accountsController.signInUser);
-exports.default = basicAuthRoutes;
